d7715e9fa4ee9876667a9e5402a4ce9ba72b3b6f
Bipan Dhakal <bipandhakal99@gmail.com>
2021-11-23 18:15:08 +0545

    webpoint internship assignment

---
+++ Third Assignment/Qno.2/script.js
@@ -0,0 +1,17 @@
+//For calculating area of triangle
+const base = prompt("Enter the base of a triangle: ");
+const height = prompt("Enter the height of a triangle: ");
+
+// calculate the area
+const area = (base * height) / 2;
+
+console.log(`The area of the triangle is ${area}`);
+
+
+//For calculating area of rectangle
+var rectangleLength = parseInt(prompt("Enter length of rectangle:"));
+var rectangleBreadth = parseInt(prompt("Enter breadth of rectangle:"));
+
+var areaOfRectangle = rectangleLength * rectangleBreadth ;
+
+console.log("Area of rectangle is: " + areaOfRectangle);
